7579848a0bbcd914abaa5c5aa7d6274f50096338
capstast <ceprieto@unal.edu.co>
2024-05-10 15:27:31 -0500

    Pruebas Streamlit para presentaacion en Dash de las series, sin terminar

---
+++ App/.ipynb_checkpoints/Streamlit_ARGOS-checkpoint.py
@@ -0,0 +1,31 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Serie Grupo Argos",
+    layout="wide",
+    initial_sidebar_state="expanded",
+    menu_items={
+        'Get Help': 'https://www.extremelycoolapp.com/help',
+        'Report a bug': "https://www.extremelycoolapp.com/bug",
+        'About': "# This is a header. This is an *extremely* cool app!"
+    }
+)
+
+
+st.title('PROYECTO DE CLASE - ANALISIS DE SERIES DE TIEMPO')
+st.header('SERIE: ANÁLISIS DE COTIZACIONES EN BOLSA - ACCIONES DE ARGOS')
+
+st.subheader('This is a subheader with a divider', divider='rainbow')
+
+st.header('This is a header with a divider', divider='rainbow')
+st.subheader('This is a subheader with a divider', divider='rainbow')
+
+st.header('This is a header with a divider', divider='rainbow')
+st.subheader('This is a subheader with a divider', divider='rainbow')
+
+st.header('This is a header with a divider', divider='rainbow')
+st.subheader('This is a subheader with a divider', divider='rainbow')
+
+st.header('This is a header with a divider', divider='rainbow')
+st.subheader('This is a subheader with a divider', divider='rainbow')
+
